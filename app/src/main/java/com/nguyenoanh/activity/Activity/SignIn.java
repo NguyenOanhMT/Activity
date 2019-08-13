ca77915ff08c2848971f326d15021e71f75e20e7
asus <meocon.on90@gmail.com>
2019-08-13 09:01:58 +0700

    upload layout Activity

--- app/src/main/java/com/nguyenoanh/activity/Activity/SignIn.java
+++ app/src/main/java/com/nguyenoanh/activity/Activity/SignIn.java
@@ -3,30 +3,69 @@ package com.nguyenoanh.activity.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.nguyenoanh.activity.R;
 
 public class SignIn extends AppCompatActivity {
     Button btnSignIn;
-    TextView tvSignUp;
+    TextView tvSignUp, tvForgotPass;
+
+    EditText edtEmail, edtPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_sign_in);
 
+        edtEmail = (EditText) findViewById (R.id.edt_email);
+        edtPass = (EditText) findViewById (R.id.edt_pass);
+
+        //receive data to activity SignUp
+        Intent intent = getIntent();
+        if (intent != null) {
+            Bundle bundle = intent.getBundleExtra(SignUp.BUNDLE);
+            if (bundle != null) {
+                edtEmail.setText(bundle.getString(SignUp.EMAIL));
+                edtPass.setText(bundle.getString(SignUp.PASS));
+            } else {
+                edtEmail.setText(intent.getStringExtra(SignUp.EMAIL));
+                edtPass.setText(intent.getStringExtra(SignUp.PASS));
+            }
+        }
+
+        //click Forgot Password -> open browser link https://www.google.com/
+        tvForgotPass = (TextView) findViewById (R.id.tv_forgot_pass);
+        tvForgotPass.setOnClickListener (new View.OnClickListener (){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com/"));
+                startActivity(intent);
+            }
+        });
+
         btnSignIn = (Button) findViewById (R.id.btn_sign_in);
         btnSignIn.setOnClickListener (new View.OnClickListener () {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(SignIn.this, NewFeed.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
-                finish();
+                String email = edtEmail.getText().toString();
+                String pass = edtPass.getText().toString();
+
+                //check email and pass if true to activity NewFeed, false still at SignIn
+                if(email.equals ("meocon.on90@gmail.com") && pass.equals ("12345678")){
+                    Intent intent = new Intent(SignIn.this, NewFeed.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                    finish();
+                }else {
+                    Toast.makeText(SignIn.this, "Login failed", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
--- app/src/main/java/com/nguyenoanh/activity/Activity/SignUp.java
+++ app/src/main/java/com/nguyenoanh/activity/Activity/SignUp.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.nguyenoanh.activity.R;
@@ -16,21 +17,27 @@ import com.nguyenoanh.activity.R;
 public class SignUp extends AppCompatActivity {
     Button btnSignUp;
     TextView tvSignIn;
+    EditText edtEmail, edtPass;
+
+    public static final String EMAIL = "email";
+    public static final String PASS = "pass";
+    public static final String BUNDLE = "bundle";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_sign_up);
 
+        edtEmail = (EditText) findViewById (R.id.edt_email);
+        edtPass = (EditText) findViewById (R.id.edt_pass);
+
         btnSignUp = (Button) findViewById (R.id.btn_sign_up);
         btnSignUp.setOnClickListener (new View.OnClickListener () {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(SignUp.this, SignIn.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity (intent);
-                finish();
-
+                byBundle ();
+//                byExtras ();
+//                finish();
             }
         });
 
@@ -44,5 +51,23 @@ public class SignUp extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    //tear up data and send it to activity SignIn
+    public void byExtras() {
+        Intent intent = new Intent(SignUp.this, SignIn.class);
+        intent.putExtra(EMAIL, edtEmail.getText().toString());
+        intent.putExtra(PASS, edtPass.getText().toString());
+        startActivity(intent);
+    }
+
+    //packed data and sent to activity SignIn
+    public void byBundle() {
+        Intent intent = new Intent(SignUp.this, SignIn.class);
+        Bundle bundle = new Bundle();
+        bundle.putString(EMAIL, edtEmail.getText().toString());
+        bundle.putString(PASS, edtPass.getText().toString());
+        intent.putExtra(BUNDLE, bundle);
+        startActivity(intent);
     }
 }
